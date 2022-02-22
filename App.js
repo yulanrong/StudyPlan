bba388e2f22c907627b3b5374380a292cbbc7787
yulanrong <yulanrong123@gmail.com>
2022-02-21 22:15:12 -0700

    studyplan

--- App.js
+++ App.js
@@ -1,11 +1,37 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import {
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  Image,
+  ImageBackground,
+  TouchableOpacity,
+  Alert,
+} from "react-native";
+import React from "react";
+import Plan from "./Plan.jsx";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <ImageBackground
+        source={require("./assets/background.jpg")}
+        imageStyle={{ opacity: 0.55 }}
+        style={styles.background}
+        resizeMode="cover"
+      >
+        <Text style={styles.title}>Your Study Plan</Text>
+        <Text style={styles.subtitle}>What do you want to study today?</Text>
+        <TouchableOpacity onPress={() => Alert.alert("click")}>
+          <Image
+            source={require("./assets/start.png")}
+            style={styles.image}
+            resizeMode="contain"
+          />
+        </TouchableOpacity>
+        <StatusBar style="auto" />
+      </ImageBackground>
     </View>
   );
 }
@@ -13,8 +39,30 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  },
+  background: {
+    flex: 1,
+    justifyContent: "center",
+  },
+  title: {
+    textAlign: "center",
+    fontSize: 40,
+    fontWeight: "bold",
+    marginBottom: 20,
+    fontFamily: "Cochin",
+    color: "#3e2321",
+  },
+  subtitle: {
+    textAlign: "center",
+    fontSize: 20,
+    fontFamily: "Cochin",
+    marginBottom: 0,
+    color: "#8f5546",
+    fontWeight: "bold",
+  },
+  image: {
+    width: 200,
+    height: 200,
+    alignSelf: "center",
   },
 });
---
+++ Plan.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Text, SafeAreaView } from 'react-native';
+
+const Plan = () => {
+
+  return (
+    <SafeAreaView>
+      <Text>
+        Study Plan
+      </Text>
+    </SafeAreaView>
+  );
+}
+
+export default Plan;
