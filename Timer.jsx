ffc2f2aa7b98f4ad750322a54e073d9855999aed
yulanrong <yulanrong123@gmail.com>
2022-03-04 00:25:41 -0700

    added coutdown timer

--- Timer.jsx
+++ Timer.jsx
@@ -24,12 +24,41 @@ const Timer = ({ route }) => {
   const [duration, setDuration] = useState(null);
   const [minutes, setMinutes] = useState(null);
   const [hasTimer, setTimer] = useState(false);
+  const [finished, setFinished] = useState(false);
+
+  let interval = useRef();
 
   const onConfirm = () => {
     if (duration > 0) {
       setMinutes(duration);
+      setTimer(true);
     }
   };
+
+  const countdownMin = () => {
+
+      setMinutes((prev) => prev - 1);
+
+  };
+  useEffect(() => {
+    if (hasTimer && minutes > 0) {
+      interval.current = setInterval(countdownMin,60000);
+    }
+    if (minutes === 0) {
+      clearInterval(interval.current);
+      setFinished(true);
+    }
+    return () => clearInterval(interval.current);
+  }, [hasTimer, minutes]);
+
+  const onPause = () => {
+    if (!pause) {
+      clearInterval(interval.current);
+    } else {
+      interval.current = setInterval(countdownMin, 1000);
+    }
+    setPause(!pause);
+  }
 
   return (
     <View style={styles.container}>
@@ -44,6 +73,9 @@ const Timer = ({ route }) => {
       <Text style={styles.subtitle} numberOfLines={3}>
         {text}
       </Text>
+      <View style={styles.content}>
+
+
       <View style={styles.setTimerStyle}>
 
 
@@ -71,6 +103,9 @@ const Timer = ({ route }) => {
         </Text>
       </Pressable>
       </View>
+      {hasTimer ?
+      <View>
+
       <View style={styles.timerLayout}>
         <Image
           source={require("./assets/timer.png")}
@@ -79,28 +114,30 @@ const Timer = ({ route }) => {
         />
         <Text style={styles.timerText}>Time Remaining: </Text>
       </View>
-      {minutes ?
+
       <View style={styles.countDownLayout}>
         <Text style={styles.countDownStyle}> {minutes} </Text>
         <Text style={styles.timerText}>Min</Text>
         </View>
-      : null
-    }
-
-      <Pressable>
-        {pause ? (
-          <Text>
+      <Pressable style={styles.pauseButton} onPress={onPause}>
+        {!pause ? (
+          <Text style={styles.confirmText}>
             {" "}
             Want a Break?{" "}
-            <MaterialIcons name="free-breakfast" size={24} color="black" />{" "}
+            <MaterialIcons name="free-breakfast" size={23} color="white" />{" "}
           </Text>
         ) : (
-          <Text>
+          <Text style={styles.confirmText}>
             Continue!{" "}
-            <FontAwesome5 name="laugh-squint" size={24} color="black" />{" "}
+            <FontAwesome5 name="laugh-squint" size={23} color="white" />{" "}
           </Text>
         )}
       </Pressable>
+        </View>
+      : null
+    }
+
+      </View>
     </View>
   );
 };
@@ -110,8 +147,7 @@ const styles = StyleSheet.create({
     padding: 4,
     alignItems: "flex-start",
     flex: 1,
-    backgroundColor: "#faebd7",
-    //justifyContent: "center",
+    backgroundColor: "#a7b9b6",
   },
   header: {
     flexDirection: "row",
@@ -133,8 +169,8 @@ const styles = StyleSheet.create({
     fontFamily: "Cochin",
     alignSelf: "center",
     padding: 10,
-    marginBottom: 30,
-    color: 'rgb(107, 60, 48)',
+    marginBottom: 10,
+    color: '#3e2321',
   },
   input: {
     fontSize: 35,
@@ -155,7 +191,7 @@ const styles = StyleSheet.create({
   },
   timerText: {
     fontSize: 25,
-    color: 'rgb(107, 60, 48)',
+    color: '#d2691e',
   },
   confirmButton: {
     margin: 10,
@@ -185,11 +221,33 @@ const styles = StyleSheet.create({
 
   },
   countDownStyle: {
-    fontSize: 45,
+    fontSize: 70,
     fontWeight: 'bold',
     alignSelf: 'center',
     color: '#3e2321'
-  }
+  },
+  content: {
+    flex: 1,
+    backgroundColor: "#faebd7",
+    marginLeft: 15,
+    marginRight: 15,
+    borderRadius: 15,
+    padding: 15,
+    marginBottom: 15,
+
+  },
+  pauseButton: {
+    margin: 10,
+    alignItems: "center",
+    justifyContent: "center",
+    alignSelf: "center",
+    paddingVertical: 10,
+    paddingHorizontal: 10,
+    borderRadius: 8,
+    backgroundColor: "#ff7f50",
+    marginBottom: 30,
+  },
+
 });
 
 export default Timer;
